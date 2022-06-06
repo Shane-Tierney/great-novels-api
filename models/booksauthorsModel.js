0abe425f14509eed0c201d96febcd9bd6c069a6d
Shane Tierney <sptierney713@gmail.com>
2022-06-05 21:15:13 -0400

    all models set up

---
+++ models/booksauthorsModel.js
@@ -0,0 +1,9 @@
+const booksauthorsModel = (connection, Sequelize, books, authors) => {
+    return connection.define('booksauthors', {
+        id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+        bookId: { type: Sequelize.INTEGER, references: { model: books, key: 'id' } },
+        authorId: { type: Sequelize.INTEGER, references: { model: authors, key: 'id' } }
+    }, { paranoid: true })
+}
+
+module.exports = { booksauthorsModel }
---
+++ models/index.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize')
+const { authorsModel } = require('./authorsModel')
+const { genresModel } = require('./genresModel')
+const { booksModel } = require('./booksModel')
+const { booksauthorsModel } = require('./booksauthorsModel')
+const { booksgenresModel } = require('./booksgenresModel')
+
+const connection = new Sequelize('bookDb', 'bookWorm', 'password123', {
+  host: 'localhost', dialect: 'mysql'
+})
+
+const authors = authorsModel(connection, Sequelize)
+const genres = genresModel(connection, Sequelize)
+const books = booksModel(connection, Sequelize)
+const booksauthors = booksauthorsModel(connection, Sequelize, books, authors)
+const booksgenres = booksgenresModel(connection, Sequelize, books, genres)
+
+authors.belongsToMany(books, { through: booksauthors })
+books.belongsToMany(authors, { through: booksauthors })
+
+genres.belongsToMany(books, { through: booksgenres })
+books.belongsToMany(genres, { through: booksgenres })
